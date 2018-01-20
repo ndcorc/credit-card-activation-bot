0186fb9885bbe15c3a7fc4d6bad5fd0f2f34c85c
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-19 18:22:25 -0600

    asdf

--- cc/components.js
+++ cc/components.js
@@ -63,7 +63,7 @@ var createComponentsServer = function(urlPath, config) {
 
     app.use(urlPath, router);
 
-    logger.info('Express server: component server created at context path=' + urlPath);
+	logger.info('* Running on http://0.0.0.0:8111' + urlPath);
 
     app.locals.endpoints = [];
     app.locals.endpoints.push({
