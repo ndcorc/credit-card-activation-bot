6bc3c23e9d777a5c6c49c9f2617105d4c9fbac9a
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-24 16:56:08 -0600

    asdf

--- cc/credit_union/read_card.js
+++ cc/credit_union/read_card.js
@@ -24,7 +24,7 @@ module.exports = {
         var options = { 
             method: 'POST',
             url: 'http://129.146.81.61:8888/read_card',
-            body: { imageUrl: JSON.stringify(cardImage) }
+            form: { imageUrl: JSON.stringify(cardImage) }
         };
 
         request(options, function (err, res, body) {
