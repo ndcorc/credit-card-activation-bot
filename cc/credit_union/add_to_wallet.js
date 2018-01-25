dc3861aac1f5330431abfd3162ad55d41bcab097
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-25 14:52:27 -0600

    asdf

--- cc/credit_union/add_to_wallet.js
+++ cc/credit_union/add_to_wallet.js
@@ -19,12 +19,6 @@ module.exports = {
     },
 
     invoke: (conversation, done) => {
-        /*
-        var options = { 
-            method: 'GET',
-            url: 'https://pscuAPI-gse00002305.apaas.em2.oraclecloud.com/add_to_wallet?mobile=' + phoneNumber,
-        };
-        */
         var cardImage = conversation.properties().cardImage;
         var logMsg = '\nAddToWallet\n______________________________________________________________\n'
         logMsg +=  '______________________________________________________________\n'+cardImage+'\n\n'
@@ -32,19 +26,8 @@ module.exports = {
 
         var wallet_badge = "https://cdn.rawgit.com/ndc466/image_bank/638df57e/ios_icons/add-to-apple-wallet-badge.png";
         var pkpass = "http://129.146.81.61:8888/download.pkpass?cc="+cardImage;
+ 
         
-        var MessageModel = conversation.MessageModel();
-        var title = "Add To Wallet";
-        var imgUrl = wallet_badge;
-        var action = MessageModel.urlActionObject(title , null, pkpass);
-        var card = MessageModel.cardObject(title, null, imgUrl, null, [action]);
-        var message = MessageModel.cardConversationMessage('horizontal', [card]);
-
-        conversation.reply(message);
-        conversation.keepTurn(false);
-        conversation.transition();
-        done();
-        /*
         var add_button = {
             "attachment": {
                 "type": "template",
@@ -74,13 +57,26 @@ module.exports = {
                 }
             }
         }
-        conversation.reply(add_button);
+        var message = MessageModel.rawConversationMessage(add_button);
+        conversation.reply(message);
+
+        conversation.keepTurn(false);
+        conversation.transition();
+        done();
+        
+
+        /*
+        var MessageModel = conversation.MessageModel();
+        var title = "Add To Wallet";
+        var imgUrl = wallet_badge;
+        var action = MessageModel.urlActionObject(title , null, pkpass);
+        var card = MessageModel.cardObject(title, null, imgUrl, null, [action]);
+        var message = MessageModel.cardConversationMessage('horizontal', [card]);
+
+        conversation.reply(message);
         conversation.keepTurn(false);
         conversation.transition();
         done();
         */
-        //var message = MessageModel.rawConversationMessage(add_button);
-        //conversation.reply(message);
-        //var message = MessageModel.attachmentConversationMessage('image', imgUrl, [action]);
     }
 }
