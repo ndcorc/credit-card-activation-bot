463e4108f0ee93e816c817ca838cb6228bd8c77a
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-25 09:42:34 -0600

    asdf

--- cc/credit_union/add_to_wallet.js
+++ cc/credit_union/add_to_wallet.js
@@ -36,8 +36,8 @@ module.exports = {
         var MessageModel = conversation.MessageModel();
         var title = "Add To Wallet";
         var imgUrl = wallet_badge;
-        var action = MessageModel.urlActionObject(null , imgUrl, pkpass);
-        var card = MessageModel.cardObject(title, null, null, null, [action]);
+        var action = MessageModel.urlActionObject(null , null, pkpass);
+        var card = MessageModel.cardObject(title, null, imgUrl, null, [action]);
         var message = MessageModel.cardConversationMessage('horizontal', [card]);
 
         /*
@@ -70,10 +70,13 @@ module.exports = {
                 }
             }
         }
-        var message = MessageModel.rawConversationMessage(add_button);
+        conversation.reply(add_button);
+        conversation.keepTurn(false);
+        conversation.transition();
+        done();
         */
-
-        //conversation.reply(add_button);
+        //var message = MessageModel.rawConversationMessage(add_button);
+        //conversation.reply(message);
         //var message = MessageModel.attachmentConversationMessage('image', imgUrl, [action]);
         conversation.reply(message);
         conversation.keepTurn(false);
